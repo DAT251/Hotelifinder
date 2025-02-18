351310fefa0c7e9cd327db1769f7d9c1ae6bd8fd
Vetle Knutsen <vetleknutsen1@gmail.com>
2025-02-18 14:24:49 +0100

    Added backend skeleton

---
+++ backend/src/main/java/dat251_gruppe2/hotelifinder/BackendApplication.java
@@ -0,0 +1,13 @@
+package dat251_gruppe2.hotelifinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
