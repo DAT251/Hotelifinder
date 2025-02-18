2574bb54f83e53b755f6d629c3e47ae35d65472d
Vetle Knutsen <vetleknutsen1@gmail.com>
2025-02-18 15:00:44 +0100

    DAT251-13 Added Controller/Model/Repo/Service packages with examples for backend.

---
+++ backend/src/main/java/dat251_gruppe2/hotelifinder/model/exampleModel.java
@@ -0,0 +1,32 @@
+package dat251_gruppe2.hotelifinder.model;
+
+public class exampleModel {
+}
+
+
+/*
+Example of User domain/class:
+
+@Entity
+@Table(name="users") // User is a reserved table name
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+
+
+    public User() {
+        this.username = "";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
+ */
