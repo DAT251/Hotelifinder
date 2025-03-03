59d4dd6a8cc3c3fc02d5dd9585872a0d09673c5b
Eivind Sulen <eivindsulen2002@gmail.com>
2025-03-03 12:01:14 +0100

    Backend: added and renamed some directories
    Added domain and dto directories among others, to follow conventional
    structure

--- backend/src/main/java/dat251_gruppe2/hotelifinder/services/exampleService.java
+++ backend/src/main/java/dat251_gruppe2/hotelifinder/services/exampleService.java
@@ -1,8 +1,6 @@
-package dat251_gruppe2.hotelifinder.service;
+package dat251_gruppe2.hotelifinder.services;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public class exampleService {
 }
-
-
