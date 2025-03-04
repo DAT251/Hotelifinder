8dcc9cf4dbd5986be84d6649023d8ad13b84e3e7
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-03-04 19:14:55 +0100

    update of components activity, header & hotel

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -1,66 +1,12 @@
+import Link from 'next/link';
 import * as React from 'react';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
 
 export function Header() {
   return (
-    <div className='m-15 flex flex-col gap-2 items-center'>
-      <h1 className='text-center'>Hotelifinder</h1>
-      <div className='flex flex-row'>
-        <ButtonGroup
-          className='SearchBG'
-          variant='text'
-          aria-label='Basic button group'
-          color='inherit'
-          size='small'
-        >
-          <Button className='Button'>Bergen</Button>
-          <Button className='Button'>Feb 19-26</Button>
-          <Button className='Button'>2 guests</Button>
-          <Button className='Button'>Search</Button>
-        </ButtonGroup>
-        {/* language + login */}
-        <div className='flex absolute right-10'>
-          <ButtonGroup
-            className='ButtonGroup'
-            variant='text'
-            aria-label='Basic button group'
-            color='inherit'
-            size='small'
-          >
-            <Button className='Button'>Language</Button>
-            <Button className='Button'>User</Button>
-          </ButtonGroup>
-        </div>
-      </div>
-      <div className='flex flex-row gap-2'>
-        {/* labels & filters */}
-        <Button className='Button' color='inherit' size='small'>
-          Prioritize Public Transportation Over Activity
-        </Button>
-        <ButtonGroup
-          className='ButtonGroup'
-          variant='text'
-          aria-label='Basic button group'
-          color='inherit'
-          size='small'
-        >
-          <Button className='Button'>Yes</Button>
-          <Button className='Button'>No</Button>
-        </ButtonGroup>
-        <Button className='Button' size='small'>
-          Preferred activities
-        </Button>
-        <Button className='Filter' size='small'>
-          Hiking
-        </Button>
-        <Button className='Filter' size='small'>
-          Shopping activities
-        </Button>
-        <Button className='Filter' size='small'>
-          Filters
-        </Button>
-      </div>
+    <div className='m-15'>
+      <Link href='/'>
+        <h1 className='text-left'>Hotelifinder</h1>
+      </Link>
     </div>
   );
 }
