b113abd14b9bce5318528fd12d89326b0c778d6f
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-03-04 19:15:21 +0100

    booking page

---
+++ frontend/src/app/booking/page.tsx
@@ -0,0 +1,23 @@
+import { Header } from '../components/header';
+
+export default function BookingPage() {
+  return (
+    <div className='flex flex-col'>
+      <Header />
+      <div className='flex flex-row justify-around'>
+        {/* left side */}
+        <div>
+          <p>How many guests?</p>
+        </div>
+        {/* right side */}
+        <div>
+          <p>Booking</p>
+          <div className='flex'>
+            <p>Hotel</p>
+            <p>Activities</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
