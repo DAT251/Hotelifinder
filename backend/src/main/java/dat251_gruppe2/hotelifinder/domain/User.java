e43b4b9d9cd1f72ebbfffed0fca8f3c8e62133ab
Vetle Knutsen <vetleknutsen1@gmail.com>
2025-03-05 15:30:29 +0100

    Added H2 database with a data.sql script to initialize some data

---
+++ backend/src/main/java/dat251_gruppe2/hotelifinder/domain/User.java
@@ -0,0 +1,69 @@
+package dat251_gruppe2.hotelifinder.domain;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Booking> bookings;
+
+    @OneToOne(mappedBy = "user")
+    private Preferences preferences;
+
+
+
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public Preferences getPreferences() {
+        return preferences;
+    }
+
+    public void setPreferences(Preferences preferences) {
+        this.preferences = preferences;
+    }
+}
