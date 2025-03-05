3b8a8d1f134ce4070071e0d37bb309316bda0ed2
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-03-05 14:37:11 +0100

    rough route

--- frontend/src/app/components/map.tsx
+++ frontend/src/app/components/map.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+'use client';
+import React, { useEffect, useState } from 'react';
+import { APIProvider, Map, useMap } from '@vis.gl/react-google-maps';
 
 const api_key = process.env.NEXT_PUBLIC_API_KEY;
 const zoom = 12;
-// Bergen latitude and longitude
 const center = { lat: 60.391, lng: 5.322 };
+
+const startLocation: string = 'Lagunen Storsenter, Bergen, Norge';
+const destLocation: string = 'Vestkanten Storsenter, Bergen, Norge';
 
 export default function MapComponent() {
   return (
@@ -16,8 +19,46 @@ export default function MapComponent() {
           defaultZoom={zoom}
           gestureHandling={'greedy'}
           disableDefaultUI={false}
-        />
+        >
+          <DirectionsRenderer />
+        </Map>
       </APIProvider>
     </div>
   );
+}
+
+function DirectionsRenderer() {
+  const map = useMap();
+  const [directions, setDirections] =
+    useState<google.maps.DirectionsResult | null>(null);
+
+  useEffect(() => {
+    if (!map) return;
+
+    const directionsService = new google.maps.DirectionsService();
+    directionsService.route(
+      {
+        origin: startLocation,
+        destination: destLocation,
+        travelMode: google.maps.TravelMode.TRANSIT,
+      },
+      (result, status) => {
+        if (status === 'OK' && result) {
+          setDirections(result);
+        }
+      }
+    );
+  }, [map]);
+
+  useEffect(() => {
+    if (!map || !directions) return;
+
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+    directionsRenderer.setDirections(directions);
+
+    return () => directionsRenderer.setMap(null);
+  }, [map, directions]);
+
+  return null;
 }
