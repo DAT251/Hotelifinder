761fe270f54056e6ca7270c0b1a39b4c6d413130
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-03-13 12:18:26 +0100

    Venue schema + fetch from backend

--- frontend/src/app/activity/page.tsx
+++ frontend/src/app/activity/page.tsx
@@ -1,18 +1,42 @@
 'use client';
 
 import Link from 'next/link';
-import { Activity } from '../components/activity';
+import { VenueCard } from '../components/venueCard';
 import { Header } from '../components/header';
-import venues from '../assets/venues.json';
+import { useEffect, useState } from 'react';
 
 export default function ActivityPage() {
+  const [venues, setVenues] = useState<Venue[]>([]);
+  const server = process.env.NEXT_PUBLIC_SERVER;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(server + '/venues');
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        const result = await response.json();
+        setVenues(result);
+      } catch (error) {
+        throw new Error('Error ' + error);
+      }
+    };
+    fetchData();
+  }, [server]);
+
   return (
     <div className='flex flex-col'>
       <Header />
       <div className='flex flex-col items-center'>
         <div className='grid grid-cols-3 gap-14'>
-          {venues.venues.map((venue, k) => (
-            <Activity key={k} name={venue.title} tags={venue.tags} />
+          {venues.map((venue, k) => (
+            <VenueCard
+              key={k}
+              name={venue.name}
+              tags={venue.tags}
+              imageUrl={venue.imageURL}
+            />
           ))}
           <div className='col-span-3' />
           <Link
