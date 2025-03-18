cf9622b9a882d6c6c7285a75adf43c64fa954ccc
Casper Karlsen <casperbkarlsen@gmail.com>
2025-03-18 14:19:43 +0100

    Placeholder algortihm

--- backend/src/main/java/dat251_gruppe2/hotelifinder/BackendApplication.java
+++ backend/src/main/java/dat251_gruppe2/hotelifinder/BackendApplication.java
@@ -1,6 +1,6 @@
 package dat251_gruppe2.hotelifinder;
 
-import dat251_gruppe2.hotelifinder.domain.User;
+import dat251_gruppe2.hotelifinder.domain.*;
 import dat251_gruppe2.hotelifinder.repositories.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +16,32 @@ public class BackendApplication {
 	UserRepository userRepository;
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+
+
+		Activity hiking = new Activity("Hiking", new Location(59.9139, 10.7522));
+		Activity swimming = new Activity("Swimming", new Location(59.9145, 10.7510));
+		Activity sightseeing = new Activity("Sightseeing", new Location(59.9200, 10.7600));
+
+		// Create some hotels
+		Hotel hotel1 = new Hotel("Vetles hus", new Location(59.9139, 10.7522));
+		Hotel hotel2 = new Hotel("Magnus hus", new Location(59.9200, 10.7600));
+
+		List<Hotel> hotels = List.of(hotel1, hotel2);
+
+		// Create a HotelRecommender
+		HotelRecommender recommender = new HotelRecommender(hotels);
+
+		// User selects activities
+		List<Activity> selectedActivities = List.of(hiking, swimming, sightseeing);
+
+		// Get recommended hotels
+		List<Hotel> recommendedHotels = recommender.recommendHotels(selectedActivities);
+
+		// Print recommended hotels
+		for (Hotel hotel : recommendedHotels) {
+			System.out.println(hotel.getName() + " - Total Travel Time: " + hotel.getTotalTravelTime() + " seconds");
+		}
+		//SpringApplication.run(BackendApplication.class, args);
 	}
 
 
