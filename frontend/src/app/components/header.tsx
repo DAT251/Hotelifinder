994891c216be90cecd94f660d428c6cd9c2c0e72
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-03-20 14:21:23 +0100

    text logo for header & favicon

--- frontend/src/app/components/header.tsx
+++ frontend/src/app/components/header.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import * as React from 'react';
+import logo from '@/assets/hotelifinder-text-logo.png';
+import Image from 'next/image';
 
 export function Header() {
   return (
     <div className='absolute m-15 top-0 left-0'>
       <Link href='/'>
-        <h1 className='text-left'>Hotelifinder</h1>
+        <Image src={logo} alt='Hotelifinder logo' height={50} />
       </Link>
     </div>
   );
