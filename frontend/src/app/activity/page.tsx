b83a2b0bef0555045816425a6556f4711d20c73d
Sander Ødegaard <sanderodegaard@158-37-234-147.hvl.no>
2025-03-21 13:44:57 +0100

    pipeline error fix questionmark

--- frontend/src/app/activity/page.tsx
+++ frontend/src/app/activity/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const dynamic = 'force-dynamic';
+
 import Link from 'next/link';
 import { VenueCard } from '@/components/venueCard';
 import { Header } from '@/components/header';
