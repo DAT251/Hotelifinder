bada7db8d22bbad716b46274e4b142819e4fcaf0
Casper Karlsen <casperbkarlsen@gmail.com>
2025-03-21 12:44:54 +0100

    Added tests

--- backend/src/main/java/dat251_gruppe2/hotelifinder/BackendApplication.java
+++ backend/src/main/java/dat251_gruppe2/hotelifinder/BackendApplication.java
@@ -3,9 +3,9 @@ package dat251_gruppe2.hotelifinder;
 import dat251_gruppe2.hotelifinder.domain.*;
 import dat251_gruppe2.hotelifinder.repositories.HotelRepository;
 import dat251_gruppe2.hotelifinder.repositories.UserRepository;
+import dat251_gruppe2.hotelifinder.services.HotelRecommender;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
