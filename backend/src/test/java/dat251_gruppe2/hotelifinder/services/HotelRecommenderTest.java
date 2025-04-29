c0b1e66b9d7f47847caf3b9666027e482d5864b2
Eivind Sulen <eivindsulen2002@gmail.com>
2025-04-29 19:37:09 +0200

    Updated HotelRecommenderTest

--- backend/src/test/java/dat251_gruppe2/hotelifinder/services/HotelRecommenderTest.java
+++ backend/src/test/java/dat251_gruppe2/hotelifinder/services/HotelRecommenderTest.java
@@ -6,8 +6,8 @@ import dat251_gruppe2.hotelifinder.domain.Hotel;
 import dat251_gruppe2.hotelifinder.domain.Location;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
-import java.io.IOException;
 import java.util.List;
 
 public class HotelRecommenderTest {
@@ -21,6 +21,8 @@ public class HotelRecommenderTest {
 
     private final List<Hotel> hotels = List.of(hotel1, hotel2);
 
+    CacheService mockCacheService = mock(CacheService.class);
+
     HotelRecommenderTest() {
         this.hiking.setLocation(new Location(59.9139, 10.7522));
         this.swimming.setLocation(new Location(59.9145, 10.7510));
@@ -28,23 +30,24 @@ public class HotelRecommenderTest {
     }
 
     @Test
-    void singleVenue() throws IOException {
+    void singleVenue() {
+
         List<Venue> selectedActivities = List.of(hiking);
 
-        HotelRecommender recommender = new HotelRecommender(this.hotels, selectedActivities);
+        HotelRecommender recommender = new HotelRecommender(mockCacheService, this.hotels, selectedActivities);
 
         Hotel bestMatch = recommender.getBestHotel();
 
-
-        assertTrue(bestMatch.getName().equals("Magnus hus"));
+        assertTrue(bestMatch.getName().equals("Vetles hus"));
         assertTrue(recommender.getTravelTime(bestMatch) <= 1);
     }
 
     @Test
-    void allHotelsRecommended() throws IOException {
+    void allHotelsRecommended() {
+
         List<Venue> selectedActivities = List.of(hiking);
 
-        HotelRecommender recommender = new HotelRecommender(this.hotels, selectedActivities);
+        HotelRecommender recommender = new HotelRecommender(mockCacheService, this.hotels, selectedActivities);
 
         List<Hotel> recommendedHotels = recommender.getHotels();
 
@@ -54,23 +57,24 @@ public class HotelRecommenderTest {
     }
 
     @Test
-    void travelTimeAboveZero() throws IOException {
+    void travelTimeAboveZero() {
 
         List<Venue> selectedActivities = List.of(hiking, swimming, sightseeing);
-        HotelRecommender recommender = new HotelRecommender(this.hotels, selectedActivities);
+
+        HotelRecommender recommender = new HotelRecommender(mockCacheService, this.hotels, selectedActivities);
 
         List<Hotel> recommendedHotels = recommender.getHotels();
-
 
         assertTrue(recommendedHotels.stream().allMatch(hotel -> recommender.getTravelTime(hotel) >= 0));
     }
 
     @Test
-    void emptyHotelList() throws IOException {
+    void emptyHotelList() {
+
         List<Hotel> hotels = List.of();
         List<Venue> selectedActivities = List.of(hiking);
 
-        HotelRecommender recommender = new HotelRecommender(hotels, selectedActivities);
+        HotelRecommender recommender = new HotelRecommender(mockCacheService, hotels, selectedActivities);
 
         List<Hotel> recommendedHotels = recommender.getHotels();
 
